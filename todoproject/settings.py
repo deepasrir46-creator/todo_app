a6fd3b2d4e3d67d40a869c24f83019a58f31771d
deepasrir46-creator <deepasrir46@gmail.com>
2026-03-03 13:08:06 +0530

    Update settings.py allowed host

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$adx^an^nmtyjjzzr7&y_s!^5t2q!$kt*n701z6rkl&q8op=(0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,5 +125,6 @@ STATICFILES_DIR=[
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
